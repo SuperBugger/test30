16a91e6dbe86c28fdb951c2b36a7ee5edfd153bb
SuperBugger <doroshenkosmirnova@mail.ru>
2024-01-17 17:35:28 +0300

    task done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="vk-widget" @scroll="handleScroll">
+      <div id="post-item" v-for="post in posts" :key="post.id">{{ post.text }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      accessToken: 'vk1.a.mICXdsnb1Wdnjf1Kr3YLCvx2H85fPujqdcdiCwp-ceftCeIzmmWPgGrNXzNVBUCBFpxR_JQlHdFP4fs3XFUU2BYcZaHF6IXBbVRAwSNv-ZeS-M3RqZOJAXBXTXlswkOZkHyc-HVwzr0CxSm52DgDWKrnhfc0-hMQjIFSrb-0NMZPJEOgEc2xBORYrz-oPBoKbAKdePOxxjFnCuptg_SJxA&expires_in=86400&user_id=331418212',
+      domain: 'anekdotikategoriib',
+      offset: 0,
+      posts: []
+    };
+  },
+  methods: {
+    async loadPosts() {
+      const callbackName = 'jsonpCallback';
+      const script = document.createElement('script');
+      script.src = `https://api.vk.com/method/wall.get?access_token=${this.accessToken}&domain=${this.domain}&count=10&offset=${this.offset}&v=5.131&callback=${callbackName}`;
+      document.body.appendChild(script);
+
+      window[callbackName] = (data) => {
+        document.body.removeChild(script);
+        delete window[callbackName];
+
+        if (data.response && data.response.items) {
+          this.posts = this.posts.concat(data.response.items);
+          this.offset += 10;
+        }
+      };
+    },
+    handleScroll() {
+      const element = document.getElementById('vk-widget');
+      if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
+        this.loadPosts();
+      }
+    }
+  },
+  mounted() {
+    const cachedPosts = localStorage.getItem('cachedPosts');
+    if (cachedPosts) {
+      this.posts = JSON.parse(cachedPosts);
+    }
+
+    this.loadPosts();
+  },
+  watch: {
+    posts() {
+      localStorage.setItem('cachedPosts', JSON.stringify(this.posts));
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#vk-widget {
+  width: 500px;
+  height: 800px;
+  overflow-y: scroll;
+  border: 0px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+#post-item {
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ddd;
+  background-color: #fff;
+  border-radius: 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 </style>
